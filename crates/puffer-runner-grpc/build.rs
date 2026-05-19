7f60d853aa77070c2d30402a3190b5d59b5fd1f8
N0zoM1z0 <1705004398@qq.com>
2026-05-19 11:46:47 +0800

    build: allow proto3 optional with older protoc

--- crates/puffer-runner-grpc/build.rs
+++ crates/puffer-runner-grpc/build.rs
@@ -2,6 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let proto = "proto/tool_runner.proto";
     println!("cargo:rerun-if-changed={proto}");
     tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
         .build_client(true)
         .build_server(true)
         .compile_protos(&[proto], &["proto"])?;
